f7639159601e8ecdb6cf046ad2f91d61bc8204ff
Innoviox <sarras305@gmail.com>
2018-02-22 18:16:01 -0500

    Added 1000 logs and a wget bash

---
+++ logs/wget_all_files.py
@@ -0,0 +1,12 @@
+import os
+
+URL = "playtak.com/games/{}.ptn"
+CMD = "wget -P wgot/ {}"
+
+def get(i):
+    run = CMD.format(URL.format(i))
+    print("Executing", CMD.format(URL.format(i)))
+    os.system(run)
+
+for i in range(1, 1000):
+    get(i)
