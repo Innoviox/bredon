2fc4ec70f20aa95d5229de8b6f027d2ce87e4c60
Innoviox <sarras305@gmail.com>
2018-02-22 18:24:01 -0500

    Removed logs; run it urself m8

--- logs/wget_all_files.py
+++ logs/wget_all_files.py
@@ -4,9 +4,9 @@ URL = "playtak.com/games/{}.ptn"
 CMD = "wget -P wgot/ {}"
 
 def get(i):
-    run = CMD.format(URL.format(i))
+    run = CMD.format(URL).format(i)
     print("Executing", CMD.format(URL.format(i)))
     os.system(run)
 
-for i in range(1, 1000):
+for i in range(1, 240789):
     get(i)
