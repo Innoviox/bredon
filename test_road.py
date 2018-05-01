5fa9e21029642ea2b7e5d0d204579d99bd6a9041
Innoviox <sarras305@gmail.com>
2018-04-30 21:56:19 -0400

    Starting exec_road

--- test_road.py
+++ test_road.py
@@ -2,6 +2,40 @@ from model import *
 
 TEST_SIZE = 4
 
+def exec_road(threat, size):
+    """
+    Threat types:
+    1) column or row (straight) e.g. "a", "1"
+    2) bent "{start}{bend-position}{new_dir}..." e.g.
+        - c1+2<b+
+    """
+
+    b = Board(size)
+    if len(threat) == 1:
+        # straight threat
+        pass
+    else:
+        srow, col, direction, *road = threat
+        row, col = tile_to_coords(srow + col)
+
+        bends, new_dirs = [], []
+        while road:
+            bends.append(road.pop(0))
+            new_dirs.append(road.pop(0))
+
+        sq = b.get(row, col)
+        for bend, nd in zip(bends, new_dirs):
+            print(direction, sq, bend, nd, sq.x, sq.y)
+            if direction in UP + DOWN:
+                cond = sq.x < int(bend) #ascii_lowercase.index(bend)
+            else:
+                cond = sq.y < int(bend)
+            if cond:
+                sq = b.get(*sq.next(direction, size))
+            else:
+                direction = nd
+            print(cond, sq)
+
 
 def test_straight_roads():
     def _test(c, z2):
